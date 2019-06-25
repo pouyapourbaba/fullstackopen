53fd88c57d4eafdd5b0b966a1bfd7106876caef9
Pouya Pourbaba <p.pourbaba@gmail.com>
2019-06-25 18:18:20 +0300

    1.11*: unicafe step6

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -14,15 +14,20 @@ const Statistics = ({ good, neutral, bad }) => {
   return (
     <div>
       <h2>statistics</h2>
-      <Statistic text="good" value={good} />
-      <Statistic text="neutral" value={neutral} />
-      <Statistic text="bad" value={bad} />
-      <Statistic text="all" value={good + neutral + bad} />
-      <Statistic text="average" value={(good - bad) / (good + neutral + bad)} />
-      <Statistic
-        text="positive"
-        value={(good / (good + neutral + bad)) * 100}
-      />
+      <table>
+        <Statistic text="good" value={good} />
+        <Statistic text="neutral" value={neutral} />
+        <Statistic text="bad" value={bad} />
+        <Statistic text="all" value={good + neutral + bad} />
+        <Statistic
+          text="average"
+          value={(good - bad) / (good + neutral + bad)}
+        />
+        <Statistic
+          text="positive"
+          value={(good / (good + neutral + bad)) * 100}
+        />
+      </table>
     </div>
   );
 };
